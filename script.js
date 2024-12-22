086d4bc0b7490e82b1902192bd6171577b9666e1
Jordan-AM <163789391+Jordan-AM@users.noreply.github.com.>
2024-12-22 15:03:44 -0300

    Play a round

--- script.js
+++ script.js
@@ -11,3 +11,27 @@ function getHumanChoice() {
     let choice = prompt("Choose between [rock], [paper] or [scissors]: ").toLowerCase()
     return choice
 }
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("It's a draw!");
+    }
+    if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log(`You won, ${humanChoice} beats ${computerChoice}`);
+    }
+    else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log(`You won, ${humanChoice} beats ${computerChoice}`);
+    }
+    else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log(`You won, ${humanChoice} beats ${computerChoice}`);
+    }
+    if (computerChoice === "rock" && humanChoice === "scissors") {
+        console.log(`You lost, ${humanChoice} loses to ${computerChoice}`);
+    }
+    else if (computerChoice === "paper" && humanChoice === "rock") {
+        console.log(`You lost, ${humanChoice} loses to ${computerChoice}`);
+    }
+    else if (computerChoice === "scissors" && humanChoice === "paper") {
+        console.log(`You lost, ${humanChoice} loses to ${computerChoice}`);
+    }
+}
